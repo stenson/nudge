769ff3941fad0f705651aeefb5dbbab547b78416
Rob Stenson <rob.stenson@gmail.com>
2022-12-20 08:19:10 -0800

    slight modernization of logo coldtype scripts

--- design/logo.py
+++ design/logo.py
@@ -1,22 +1,21 @@
 from coldtype import *
 
 fnt = Font.Find("IrregardlessV")
-fnt2 = Font.Find("CoFoPeshkaV0.4_Variable")
+fnt2 = Font.Find("CoFoPeshkaVariableV1.0")
 
 @renderable((256, 256))
 def logo(_r):
     r = Rect(1080, 1080)
-    return (PS([
-        (P().oval(r.inset(50))
-            .f(hsl(0.4, 0.6, 0.5))),
-        (StSt("Nudge".upper(), fnt, 550, wght=0.75, slnt=1, ss01=1)
+    return (P(
+        P().oval(r.inset(50)).f(hsl(0.4, 0.6, 0.5)),
+        StSt("Nudge".upper(), fnt, 550, wght=0.75, slnt=1, ss01=1)
             .align(r)
             .pen()
             .f(1)
             .rotate(5)
-            .translate(0, 30)),
-        (StSt("+/-", fnt2, 150, wght=0.5, wdth=1)
+            .translate(0, 30),
+        StSt("+/-", fnt2, 150, wght=0.5, wdth=1)
             .align(r.take(450, "mny"))
-            .f(1))])
+            .f(1))
         .scale(_r.w/1080)
         .align(_r))
--- design/rslogo.py
+++ design/rslogo.py
@@ -2,10 +2,8 @@ from coldtype import *
 
 fnt = Font.Find("IrregardlessV")
 
-@renderable((128*2, 128*2))
+@renderable((128*2, 128*2), bg=hsl(0.7), render_bg=1)
 def logo(r):
-    return DPS([
-        DP(r).f(hsl(0.7)),
-        (StSt("RS", fnt, 250, wght=0.25, slnt=1, ss03=1)
-            .align(r)
-            .f(1))])
+    return (StSt("RS", fnt, 250, wght=0.25, slnt=1, ss03=1)
+        .align(r)
+        .f(1))
